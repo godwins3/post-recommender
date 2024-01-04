ba8763711eda50420cd4ab3a5f5cb9f4022731b5
godwins3 <praisegodwins4@gmail.com>
2024-01-04 11:45:26 +0300

    create setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A social media recommendation system',
+    author='Tamu Technologies',
+    license='MIT',
+)
